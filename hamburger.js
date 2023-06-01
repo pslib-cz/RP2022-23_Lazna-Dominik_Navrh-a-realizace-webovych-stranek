0e9304634292ba1be134761ae1a0ed39981bfb20
Dominik Lazna <dominik.lazna.04@gmail.com>
2023-06-01 17:27:12 +0200

    Oprava hamburgeru

--- hamburger.js
+++ hamburger.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let hamelmnts = document.querySelectorAll(".nav__links");
+    let hamelmnts = document.querySelectorAll(".nav__odkazy");
     for (const btn of document.querySelectorAll(".nav__hamburger")) {
         btn.addEventListener("click", (e) => {
             e.preventDefault();
